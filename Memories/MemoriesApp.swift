e178ffb9c853f522494b9bdbcbc797e81402961f
Paul Nicolet <paul@getluko.com>
2023-05-05 14:27:08 +0200

    Initial Commit

---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Paul Nicolet on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
