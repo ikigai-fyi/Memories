612770b883c64644c70348bba515cd8eee8180d1
Vincent Ballet <vincent.ballet@me.com>
2023-05-14 18:28:47 +0200

    Fix layout constraints on the homeview

--- Memories/MemoriesHomeView.swift
+++ Memories/MemoriesHomeView.swift
@@ -14,80 +14,97 @@ struct MemoriesHomeView: View {
     
     
     var body: some View {
-        ScrollView{
-            VStack {
-                
-                // Spacer -----------------------------------------------------
-                Spacer()
-                    .frame(minHeight: 10, idealHeight: 40, maxHeight: 80)
-                    .fixedSize()
-                
-                // Header ----------------------------------------------------
-                HStack {
-                    AsyncImage(url: URL(string: viewModel.pictureUrl)) { image in
-                        image
-                    } placeholder: {
-                        Color.gray.opacity(0.1)
-                    }
-                    .frame(width: 64, height: 64)
-                    .cornerRadius(50)
+        GeometryReader { proxy in
+            ScrollView{
+                VStack {
+                   
                     
-                    VStack(alignment: .leading) {
-                        Text(viewModel.firstName)
-                        Text(viewModel.lastName)
-                    }
-                }
-                
-                // Spacer -----------------------------------------------------
-                Spacer()
-                
-                // Activity widget -----------------------------------------------------
-                if let activity = viewModel.activity {
-                    VStack {
-                        MemoriesWidgetView(activity: activity)
-                            .frame(width: 292, height: 311)
-                            .background(.gray.opacity(0.1))
-                            .cornerRadius(12)
+                    // Spacer -----------------------------------------------------
+                    Spacer()
+                        .frame(minHeight: 10, idealHeight: 40, maxHeight: 80)
+                        .fixedSize()
+                    
+                    // Header ----------------------------------------------------
+                    HStack {
+                        AsyncImage(url: URL(string: viewModel.pictureUrl)) { image in
+                            image
+                        } placeholder: {
+                            Color.gray.opacity(0.1)
+                        }
+                        .frame(width: 64, height: 64)
+                        .cornerRadius(50)
+                        .shadow(radius: 8)
                         
-                        Text("Your widget preview").font(.subheadline)
+                        VStack(alignment: .leading) {
+                            Text(viewModel.firstName)
+                            Text(viewModel.lastName)
+                        }
+                    }.frame(height: 100)
+                    
+                    // Spacer -----------------------------------------------------
+                    Spacer()
+                    
+                    VStack{
+                        // Activity widget -----------------------------------------------------
+                        if let activity = viewModel.activity {
+                            VStack {
+                                MemoriesWidgetView(activity: activity)
+                                    .frame(width: 292, height: 311)
+                                    .background(.gray.opacity(0.1))
+                                    .cornerRadius(12)
+                                    .shadow(radius: 12)
+                                
+                                Text("Your widget preview").font(.subheadline)
+                            }
+                        } else {
+                            ProgressView()
+                                .frame(width: 292, height: 311)
+                                .background(.gray.opacity(0.1))
+                                .cornerRadius(12)
+                                .shadow(radius: 8)
+                            
+                        }
+                    }.frame(height: 400)
+                    
+                    // Spacer -----------------------------------------------------
+                    Spacer()
+                    
+                    // Add widget button -----------------------------------------------------
+                    VStack{
+                        Button {
+                            isShowingWebView = true
+                        } label: {
+                            Label {
+                                Text("Add widget")
+                                
+                                    .bold()
+                            } icon: {
+                                Image(systemName: "plus.circle.fill")
+                            }.padding()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(35)
+                        .sheet(isPresented: $isShowingWebView) {
+                            WebView(url: URL(string: "https://support.apple.com/en-us/HT207122")!)
+                        }
                     }
-                } else {
-                    ProgressView()
+                    .padding()
+                   
                 }
-                
-                // Spacer -----------------------------------------------------
-                Spacer()
-                
-                // Add widget button -----------------------------------------------------
-                Button {
-                    isShowingWebView = true
-                } label: {
-                    Label {
-                        Text("Add widget")
-                            .bold()
-                    } icon: {
-                        Image(systemName: "plus.circle.fill")
-                    }.padding()
+                .frame(maxWidth: .infinity, minHeight: proxy.size.height)
+                .onAppear {
+                    Task {
+                        await viewModel.fetchRandomActivity()
+                    }
                 }
-                .frame(maxWidth: .infinity)
-                .background(.blue)
-                .foregroundColor(.white)
-                .cornerRadius(35)
-                .sheet(isPresented: $isShowingWebView) {
-                    WebView(url: URL(string: "https://support.apple.com/en-us/HT207122")!)
-                }
-                
+            } // scrollview
+            .refreshable {
+                await viewModel.fetchRandomActivity()
             }
-            .frame(maxWidth: .infinity)
-            .padding(32.0)
-            .onAppear {
-                Task {
-                    await viewModel.fetchRandomActivity()
-                }
-            }
-        }.refreshable {
-            await viewModel.fetchRandomActivity()
-        }
+        } // geometryreader
+        
         
     }
 }
