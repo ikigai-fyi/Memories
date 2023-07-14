975974f63277162a94a6696a2acf37233358d5fc
Paul Nicolet <nicoletpaul@orange.fr>
2023-07-14 16:28:10 +0200

    Update pods

--- Podfile
+++ Podfile
@@ -5,8 +5,6 @@ target 'Memories' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'AmplitudeSwift', '~> 0.4.4'
-  pod "PostHog", "~> 1.0"
   pod 'Sentry', :git => 'https://github.com/getsentry/sentry-cocoa.git', :tag => '8.7.3'
 end
 
@@ -20,7 +18,6 @@ target 'Activity' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'AmplitudeSwift', '~> 0.4.4'
-  pod "PostHog", "~> 1.0"
-
+  pod 'AmplitudeSwift', '~> 0.4.6'
+  pod 'PostHog', '~> 1.1'
 end
--- Pods/Target Support Files/Pods-Memories/Pods-Memories-frameworks.sh
+++ Pods/Target Support Files/Pods-Memories/Pods-Memories-frameworks.sh
@@ -176,16 +176,16 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "dev" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PostHog/PostHog.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SentryPrivate/SentryPrivate.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PostHog/PostHog.framework"
 fi
 if [[ "$CONFIGURATION" == "prod" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PostHog/PostHog.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SentryPrivate/SentryPrivate.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PostHog/PostHog.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
