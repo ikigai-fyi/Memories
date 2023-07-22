d3d6413d38a98c710e5f8d552744c0f6ba0b5a22
Paul Nicolet <nicoletpaul@orange.fr>
2023-07-22 15:36:13 +0200

    Add comment

--- Activity/FakeBehaviour.swift
+++ Activity/FakeBehaviour.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// Define behaviours to ease dev experience
+/// This works only because JWTs have infinite lifetime as of today
 public enum FakeBehaviour {
     case noActivity
     case noPicture
@@ -20,8 +22,12 @@ public enum FakeBehaviour {
     
     public var jwt: String {
         switch self {
-        case .noActivity: return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5MDAyOTY3MSwianRpIjoiZTBmNDVlMWEtNThjMi00NjNhLThiZDYtNTJmMzA3NWI4NTVhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImF0aF9pc1czYVhXSGo0eGREWGdOIiwibmJmIjoxNjkwMDI5NjcxfQ.1Oyz_dV0gdQ6MiaaMk7XA6khN6a_lA3ldv_kg5YR03c"
-        case .noPicture: return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5MDAyOTc2NCwianRpIjoiMWEyNjBmMDItYWUxOS00ODExLWJmNjQtYmFhNmE4NTBlNzA4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImF0aF9uWUN5RzJ2VDdHbWFuNTJ1IiwibmJmIjoxNjkwMDI5NzY0fQ.t-tbxZvYUww9L7u9NflCbbZr_YIgxRyrI8SiBWceUkE"
+        case .noActivity:
+            // Strava user: "nicoletpaul+teststrava+noactivity@..."
+            return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5MDAyOTY3MSwianRpIjoiZTBmNDVlMWEtNThjMi00NjNhLThiZDYtNTJmMzA3NWI4NTVhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImF0aF9pc1czYVhXSGo0eGREWGdOIiwibmJmIjoxNjkwMDI5NjcxfQ.1Oyz_dV0gdQ6MiaaMk7XA6khN6a_lA3ldv_kg5YR03c"
+        case .noPicture:
+            // Strava user: "nicoletpaul+teststrava+nopicture@..."
+            return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5MDAyOTc2NCwianRpIjoiMWEyNjBmMDItYWUxOS00ODExLWJmNjQtYmFhNmE4NTBlNzA4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImF0aF9uWUN5RzJ2VDdHbWFuNTJ1IiwibmJmIjoxNjkwMDI5NzY0fQ.t-tbxZvYUww9L7u9NflCbbZr_YIgxRyrI8SiBWceUkE"
         }
     }
 }
