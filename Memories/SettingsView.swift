95b17499c700eeff38ff6ec936aa1f7d53649a03
Paul Nicolet <nicoletpaul@orange.fr>
2023-11-23 18:14:14 +0100

    Make refresh rate synced with backend

--- Memories/SettingsView.swift
+++ Memories/SettingsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Activity
 import Crisp
+import Sentry
 
 struct SettingsView: View {
     @EnvironmentObject var activityViewModel: ActivityViewModel
@@ -20,8 +21,9 @@ struct SettingsView: View {
     // state
     @State private var showingFakeBehaviourAlert = false
     @State private var isShowingAlert = false
-    @AppStorage(UserDefaultsKeys.userWidgetRefreshRatePerDay) var widgetRefreshRatePerDay: Int = Helper.getUserWidgetRefreshRatePerDay()!
     @State private var measurementSystemString: String = Helper.getIsUserUsingMetricSystemFromUserDefaults()! ? NSLocalizedString("Metric", comment: "comment") : NSLocalizedString("Imperial", comment: "comment")
+    @StateObject private var remoteSettings = RemoteSettings()
+    
     
     
     var body: some View {
@@ -97,17 +99,33 @@ struct SettingsView: View {
                         )
                     }
                     
-                    Stepper(String(format: NSLocalizedString("Refresh %dx per day", comment: "comment"), widgetRefreshRatePerDay), value: $widgetRefreshRatePerDay, in: 1...4, step: 1){_ in
-                        Helper.saveUserWidgetRefreshRatePerDay(refreshRatePerDay: widgetRefreshRatePerDay)
+                    
+                    
+                    Stepper(String(format: NSLocalizedString("Refresh %dx per day", comment: "comment"), self.remoteSettings.widgetRefreshRatePerDay ?? 0)) {
+                        
+                        Task {
+                            await self.remoteSettings.incrementRefreshRate()
+                        }
                         
                         Analytics.capture(
                             event: .updateSettingRefreshRatePerDay,
-                            eventProperties: [.settingValue: widgetRefreshRatePerDay],
-                            userProperties: [.refreshRatePerDay: widgetRefreshRatePerDay]
+                            eventProperties: [.settingValue: self.remoteSettings.widgetRefreshRatePerDay!],
+                            userProperties: [.refreshRatePerDay: self.remoteSettings.widgetRefreshRatePerDay!]
                         )
-                    }
+                    } onDecrement: {
+                        Task {
+                            await self.remoteSettings.decrementRefreshRate()
+                        }
+                        
+                        Analytics.capture(
+                            event: .updateSettingRefreshRatePerDay,
+                            eventProperties: [.settingValue: self.remoteSettings.widgetRefreshRatePerDay!],
+                            userProperties: [.refreshRatePerDay: self.remoteSettings.widgetRefreshRatePerDay!]
+                        )
+                    }.disabled(self.remoteSettings.isLoading)
                     
                 }
+                
                 
                 Section(header: Text("Contact us")){
                     Button("Suggest features") {
@@ -143,9 +161,94 @@ struct SettingsView: View {
                 Text("Done").bold()
             })
             
-            
-            
+        }.task {
+            await self.remoteSettings.fetch()
         }
+    }
+}
+
+struct Settings: Codable {
+    let refreshPeriodInHours: Int
+}
+
+class RemoteSettings: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var widgetRefreshRatePerDay: Int? = nil
+    
+    @MainActor
+    func incrementRefreshRate() async {
+        guard self.widgetRefreshRatePerDay! < 4 else { return }
+        self.widgetRefreshRatePerDay! += 1
+        await self.patch()
+    }
+    
+    @MainActor
+    func decrementRefreshRate() async {
+        guard self.widgetRefreshRatePerDay! > 1 else { return }
+        self.widgetRefreshRatePerDay! -= 1
+        await self.patch()
+    }
+    
+    @MainActor
+    func fetch() async {
+        self.isLoading = true
+            
+        let url = URLComponents(string: "\(Config.backendURL)/rest/settings")!
+        let jwt = StravaLoginViewModel.getAthleteFromUserDefault()!.jwt
+        var request = URLRequest(url: url.url!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(jwt)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(.standard)
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                do {
+                    let decoded = try decoder.decode(Settings.self, from: data)
+                    self.widgetRefreshRatePerDay = 24 / decoded.refreshPeriodInHours
+                } catch {
+                    SentrySDK.capture(error: error)
+                }
+            } else {
+                SentrySDK.capture(message: response.description)
+            }
+        } catch {
+            SentrySDK.capture(error: error)
+        }
+
+
+        self.isLoading = false
+    }
+    
+    @MainActor
+    private func patch() async {
+        self.isLoading = true
+        
+        let url = URLComponents(string: "\(Config.backendURL)/rest/settings")!
+        let jwt = StravaLoginViewModel.getAthleteFromUserDefault()!.jwt
+        var request = URLRequest(url: url.url!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(jwt)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "PATCH"
+        let parameters: [String: Any] = [
+            "refresh_period_in_hours": 24 / self.widgetRefreshRatePerDay!
+        ]
+        request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        
+        do {
+            let (_, response) = try await URLSession.shared.data(for: request)
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                SentrySDK.capture(message: response.description)
+            }
+        } catch {
+            SentrySDK.capture(error: error)
+        }
+        
+        self.isLoading = false
     }
 }
 
